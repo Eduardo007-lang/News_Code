b4daa8479b90e0b06f220f0ca35a07351019238a
Eduardo007-lang <Eduardo2018ads@hotmail.com>
2020-09-01 22:31:27 -0300

    Fiz uma linha de codigo pra teste

---
+++ Teste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace News_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Comitando pro repositorio remoto!");
+        }
+    }
+}
