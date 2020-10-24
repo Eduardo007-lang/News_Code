4c005d64292104710654a44546b1d47ad9ca85ff
Eduardo007-lang <Eduardo2018ads@hotmail.com>
2020-10-24 16:02:28 -0300

    Criado sistema de multas

---
+++ Curso.JS/Aula_11.JS/ex008.js
@@ -0,0 +1,10 @@
+//variavel vel
+var vel = 60
+//Escrevendo na tela
+console.log(`A velocidade do seu carro é ${vel}KM/H`)
+//condição simples 
+if (vel > 60) {
+    console.log('Voce será multado')
+}
+//Escrevendo na tela
+console.log('Dirija sempre usando o cinto de segurança')
