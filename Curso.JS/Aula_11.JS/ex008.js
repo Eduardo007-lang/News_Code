fadcf9dbb5e8430bb5e4ec6395bca08678e52364
Eduardo Pereira <53060876+Eduardo007-lang@users.noreply.github.com>
2020-10-30 14:08:41 -0300

    Atividade 11

--- Curso.JS/Aula_11.JS/ex008.js
+++ Curso.JS/Aula_11.JS/ex008.js
@@ -1,10 +1,8 @@
-//variavel vel
-var vel = 60
-//Escrevendo na tela
-console.log(`A velocidade do seu carro é ${vel}KM/H`)
-//condição simples 
-if (vel > 60) {
-    console.log('Voce será multado')
-}
-//Escrevendo na tela
-console.log('Dirija sempre usando o cinto de segurança')
+    var vel = 60
+    console.log(`A velocidade do seu carro é ${vel}km/h`)
+    if(vel > 60){
+        console.log('Voce será multado!!!')
+    }
+
+    console.log('Dirija com cuidado!!')
+
